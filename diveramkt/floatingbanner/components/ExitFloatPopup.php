aa9cc462608242ebaf5e0bc2c6f7fffd3ea5c719
fredec <fredericomarinho@hotmail.com>
2020-03-19 21:12:52 -0300

    the banner does not appear in the page linked to the banner

--- diveramkt/floatingbanner/components/ExitFloatPopup.php
+++ diveramkt/floatingbanner/components/ExitFloatPopup.php
@@ -20,8 +20,28 @@ class ExitFloatPopup extends ComponentBase
 		];
 	}
 
+	public function link($url) {
+		if(!strpos("[".$url."]", "http://") && !strpos("[".$url."]", "https://")) $url='http://'.$url;
+		return $url;
+	}
+
+	public function target($link){
+		if(!strpos("[".$link."]", $_SERVER['HTTP_HOST'])) return '_blank';
+		else return '_parent';
+	}
+
 	public function onRun(){
 		$this->exitpopup = $this->getExitPopup();
+
+		// $base_url = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') . '://' . $_SERVER['HTTP_HOST'];
+		// $valores['target']=$this->target($valores['link'], $base_url);
+
+		if(isset($this->exitpopup->attributes)){
+			$valores=$this->exitpopup->attributes;
+			if(str_replace(' ','',$valores['link']) != '') $valores['link']=$this->link($valores['link']);
+			$valores['target']='_parent'; if(!strpos("[".$valores['link']."]", $_SERVER['HTTP_HOST'])) $valores['target']='_blank';
+			$this->exitpopup->attributes=$valores;
+		}
 	}
 
 	protected function getExitPopup(){
--- diveramkt/floatingbanner/components/FloatPopup.php
+++ diveramkt/floatingbanner/components/FloatPopup.php
@@ -1,5 +1,6 @@
 <?php namespace Diveramkt\FloatingBanner\Components;
 
+use Cache;
 use Cms\Classes\ComponentBase;
 
 use Diveramkt\FloatingBanner\Models\Popup;
@@ -21,11 +22,51 @@ class FloatPopup extends ComponentBase
 	}
 
 	public function onRun(){
+
+		if($_SERVER['REDIRECT_URL'] == '/boletim-especial-2019') return;
+
 		$this->popup = $this->getPopup();
+		if(isset($this->popup[0])) $this->popup=$this->popup[0];
+
+		if(isset($this->popup->attributes) && isset($this->popup->attributes['dias_oculto']) && $this->popup->attributes['dias_oculto'] > 0){
+
+			$name='banner_float_'.serialize($this->popup->attributes);
+			$name= preg_replace("/[^a-zA-Z0-9]/", "", $name);
+
+			if (Cache::has($name)) {
+				$veri=Cache::get($name);
+				$this->popup='';
+			}else{
+				Cache::pull($name);
+				$dias=$this->popup->attributes['dias_oculto']*(24*60);
+				Cache::add($name, 'carregado', $dias);
+			}
+
+		}
+
+		if(isset($this->popup->attributes)){
+			$this->popup->attributes['target']='_parent';
+			$url=$this->popup->attributes['link'];
+			if(!strpos("[".$url."]", "http://") && !strpos("[".$url."]", "https://")) $url='http://'.$url;
+			$this->popup->attributes['link']=$url;
+
+			if(!strpos("[".$this->popup->attributes['link']."]", $_SERVER['HTTP_HOST'])) $this->popup->attributes['target']='_blank';
+		}
+		
 	}
 
 	protected function getPopup(){
-		return Popup::first();
+		// return Popup::first();
+
+		$retorno=Popup::where('data_entrada','<=',date('Y-m-d H:i:s'))->whereNull('data_saida')
+		->orWhere('data_entrada','<=',date('Y-m-d H:i:s'))->where('data_saida','0000-00-00')
+		->orWhere('data_entrada','<=',date('Y-m-d H:i:s'))->where('data_saida','>',date('Y-m-d H:i:s'))
+		->get();
+
+		$veri_link=explode($_SERVER['HTTP_HOST'], $retorno[0]->link); $veri_link=end($veri_link);
+		if($_SERVER['REDIRECT_URL'] == $veri_link) $retorno=false;
+
+		return $retorno;
 	}
 
 	public $popup;
